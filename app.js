c2d4665c180c83a10e4fe752bc0d8b7645babae8
prasannalakshmiy <lakshmi.prasanna073@gmail.com>
2018-02-14 12:42:30 +0530

    Prototypical inheritance

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-var greet1 = require('./greet.js')
+function Person(firstName, lastName){
 
-greet1();
+this.firstName = firstName;
+this.lastName = lastName;
+
+}
+
+Person.prototype.greet = function(){
+  return 'Hello'+' '+ this.firstName+' '+ this.lastName
+}
+
+var John = new Person('John', 'Doe');
+console.log(John.firstName);
+console.log(John.greet());
+var Jean = new Person('Jane', 'Doll');
+console.log(John.__proto__); // this lets us see on what function we are working with
+
+
--- greet.js
+++
@@ -1,5 +0,0 @@
-var greet = function(){
-    console.log("hello")
-};
-
-module.exports = greet; 
